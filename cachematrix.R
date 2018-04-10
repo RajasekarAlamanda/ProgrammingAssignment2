1d1e1760edf612bd7256b0d6f77524031d20e468
RajasekarA <rajasekar.alamanda@gmail.com>
2018-04-10 18:00:34 -0400

    Week 2 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -12,28 +12,40 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
   get <- function() x
-  setInverse <- function(mean) m <<- mean
-  getInverse <- function() m
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+
 
 ## Write a short comment describing this function
 ## To get the inverse of the matrix, "makeCacheMatrix" from the steps above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then it should retrieve the inverse from the cache.
 
+
 cacheSolve <- function(x, ...) {
-  inv <- x$getInverse()
+  inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
   data <- x$get()
-  inv <- solve(mat, ...)
-  x$setmean(m)
-  m
-  inv <- solve(data)
+  inv <- solve(data, ...)
   x$setinverse(inv)
   inv
 }
+
+### Solution:
+#mat <- matrix(c(1,3,5,6), nrow=2, ncol=2, byrow=TRUE)
+#mat
+##  [,1] [,2]
+## [1,]    1    3
+## [2,]    5    6
+#mat1<- makeCacheMatrix(mat)
+#cacheSolve(mat1)
+##            [,1]       [,2]
+## [1,] -0.6666667  0.3333333
+## [2,]  0.5555556 -0.1111111
